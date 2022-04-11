3e4dc481bdfe3b6159358e2e74bda9985cd6ff08
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-04-11 16:23:34 +0300

    rest-assured

---
+++ src/test/java/ru/gb/AppTest.java
@@ -0,0 +1,20 @@
+package ru.gb;
+
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        given()                                     //в rest-assured все тесты начинаются с given()
+        .when()
+        .then()
+
+
+
+    }
+}
